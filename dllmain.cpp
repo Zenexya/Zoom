8732b7038c3ac7c660e864289f6c18fcc04c1896
xFelienDev <101019279+xFelienDev@users.noreply.github.com>
2022-04-29 12:59:20 +0200

    Add files via upload

---
+++ dllmain.cpp
@@ -0,0 +1,49 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "pch.h"
+#include <iterator>
+#include <Windows.h>
+#include <vector>
+#include <iostream>
+#include <sstream>
+#include <string.h>
+#include <thread>
+DWORD Proc;
+DWORD PID;
+using namespace std;
+
+void print(const char* text) {
+	std::cout << text, "\n";
+}
+
+void Start() {
+	const auto freecon = &FreeConsole;
+	DWORD old;
+	VirtualProtect(freecon, 1, PAGE_EXECUTE_READWRITE, &old);
+	*reinterpret_cast<std::uint8_t*>(freecon) = 0xC3;
+	VirtualProtect(freecon, 1, old, &old);
+	AllocConsole();
+	FILE* file_stream;
+	freopen_s(&file_stream, "CONIN$", "r", stdin);
+	freopen_s(&file_stream, "CONOUT$", "w", stdout);
+	freopen_s(&file_stream, "CONOUT$", "w", stderr);
+	SetConsoleTitleA("Zoom");
+}
+
+int main() {
+	Start();
+	print("Injected");
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule,
+	DWORD  ul_reason_for_call,
+	LPVOID lpReserved
+)
+{
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+		std::thread(main).detach();
+		break;
+	}
+	return TRUE;
+}
